c7b9e5a919fdf795f1ab4652cfa7825ea0cd9471
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-09-07 04:06:45 +0800

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -120,7 +120,7 @@ var Game = {}
 // Every constant variable is saved here
 Game.GameConst = {
   "priceMultiplier": 2,
-  "VERSION": "all members reached 100000 shiba ruther tumanda,jeryan royate,alvin reyes ,jasmin glinoga,abby valdez"
+  "VERSION": "all members reached 100000 shiba ruther tumanda,jeryan royate,alvin reyes ,jasmin glinoga,abby valdez and members reached 10000 shiba,liza elarmo"
 }
 
 Game.units = [
